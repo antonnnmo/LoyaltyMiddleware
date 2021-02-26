d166fd4f2f3deb385a4f340b67bde1d0bdb3d4c8
Dmitriy Orlov <orlov@samarasoft.com>
2021-02-26 14:19:48 +0400

    https://tracker.samaralab.ru/board?work=TEAM%2B5462

--- LoyaltyMiddleware/MiddlewareHandlers/CalculateHandler.cs
+++ LoyaltyMiddleware/MiddlewareHandlers/CalculateHandler.cs
@@ -1,6 +1,8 @@
 ﻿using RedmondLoyaltyMiddleware.Models.InternalDB;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json.Linq;
+using System;
 
 namespace LoyaltyMiddleware.MiddlewareHandlers
 {
@@ -8,6 +10,36 @@ namespace LoyaltyMiddleware.MiddlewareHandlers
 	{
 		public Dictionary<string, object> GetHandledResponse(Dictionary<string, object> requestData, Dictionary<string, object> responseData, Dictionary<string, object> additionalResponseData, MiddlewareDBContext dbContext)
 		{
+			if (responseData.ContainsKey("data"))
+			{
+				JObject data = responseData["data"] as JObject;
+				if (data.ContainsKey("productDiscounts"))
+				{
+					JArray productDiscounts = data["productDiscounts"] as JArray;
+					if (productDiscounts != null && productDiscounts.Count > 0)
+					{
+						foreach (JObject productDiscount in productDiscounts)
+						{
+							var discountAmount = 0;
+							JArray discounts = productDiscount["discounts"] as JArray;
+							if (discounts != null && discounts.Count > 0)
+							{
+								foreach (JObject discount in discounts)
+									if (discount.ContainsKey("discount"))
+									{
+										var d = Convert.ToInt32(Math.Round((decimal)discount["discount"]));
+										discount["discount"] = d;
+										discountAmount += d;
+									}
+							}
+							if (productDiscount.ContainsKey("discount"))
+								productDiscount["discount"] = discountAmount;
+						}
+					}
+				}
+			}
+
+
 			if (additionalResponseData != null) 
 			{
 				responseData = new Dictionary<string, object>[] { responseData, additionalResponseData }.SelectMany(dict => dict)
